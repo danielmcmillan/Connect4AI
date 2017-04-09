f1c836550748e834bcd3666deb76f6417a6f0924
Daniel McMillan <mcmillan@hotmail.com.au>
2017-04-09 12:05:48 +1000

    Board documentation

--- src/board.h
+++ src/board.h
@@ -6,13 +6,17 @@
 
 namespace ConnectFour
 {
+	/// @class Board
+	/// @brief Class for representing and manipulating the state of a Connect Four board.
 	class Board
 	{
 	public:
+		/// @brief Board width
 		static const int width = 7;
+		/// @brief Board height
 		static const int height = 6;
 
-		// Swap turns with the other player (i.e. swap all pieces)
+		/// @brief Swap turns with the other player (i.e. swap all pieces).
 		void swap()
 		{
 			currentPlayer ^= otherPlayer;
@@ -20,46 +24,52 @@ namespace ConnectFour
 			currentPlayer ^= otherPlayer;
 		}
 
-		// Sets whether the specified space is occupied by the current player or empty
+		/// @brief Sets whether the specified space is occupied by the current player or empty.
 		void setSpace(int column, int row, bool occupied);
 
-		// Clear all pieces of both players from the board
+		/// @brief Clear all pieces of both players from the board.
 		void clear() { currentPlayer = 0; otherPlayer = 0; }
 
-		// Check whether the current player has connected 4
+		/// @brief Check whether the current player has connected 4.
 		bool isWin() const;
 
-		// Get the total number of pieces for the current player
+		/// @brief Get a count of the current player's pieces on the board.
 		int count() const { return currentPlayer.count(); }
 
-		// Check whether playing in the specified column is a legal move.
-		// Column must be valid.
+		/// @brief Check whether playing in the specified column is a legal move.
+		/// @param column The column to check. Must be a valid column.
 		bool canPlay(int column) const;
 
-		// Play in the specified column (must be legal)
+		/// @brief Play in the specified column.
+		/// @param column The column to play in. It must exist and canPlay(column) must be true.
 		void play(int column);
 
-		// Count the number connections of different sizes
-		// Returns an array: { exactly 2, exactly 3, atleast 4 }
+		/// @brief Array for storing the number of connections of size 2 and 3.
 		typedef std::tr1::array<int, 3> connectionsArray;
+
+		/// @brief Count the numbers of any connection of size 2, 3 and 4+.
+		/// @return A connectionsArray: { exactly 2, exactly 3, atleast 4 }.
 		connectionsArray countConnections() const;
 
-		// Return a string representation for the board with red as the current player
+		/// @brief Get a string representation for the board with red as the current player.
+		/// @param row Optional parameter specifying a single row to limit the description to.
 		std::string getDescription(int row = -1) const;
 
-		// Set the board state based on the given description
+		/// @brief Set the board state based on the given description
+		/// @param description String description of a board. invalid_argument is thrown if it is invalid.
 		void setFromDescription(const std::string &description);
 
+		/// @brief Output board descriptions from a Board into an output stream.
 		friend std::ostream& operator<<(std::ostream& os, const Board& b);
+
+		/// @brief Read board descriptions from an input stream into a Board.
 		friend std::istream& operator>>(std::istream& is, Board& b);
 
-	protected:
-		/*
-		 * Data representing the positions of each of the players pieces.
-		 * A 1 represents piece existing, 0 represents no piece (for the player).
-		 * Order of bits is row-major order with most significant bit representing the lower left piece.
-		 * There is a zero bit after each row to avoid erroneous matches between adjacent rows.
-		*/
+	private:
+		// Data representing the positions of each of the players pieces.
+		// A 1 represents piece existing, 0 represents no piece (for the player).
+		// Order of bits is row-major order with most significant bit representing the lower left piece.
+		// There is a zero bit after each row to avoid erroneous matches between adjacent rows.
 		typedef std::bitset<(width + 1)*height> bitset;
 		bitset currentPlayer, otherPlayer;
 
